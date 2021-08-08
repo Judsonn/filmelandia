115ea33edebba9d988f3259fdd7185bda168a079
Judson Henrique <judson.henrique01@gmail.com>
2021-08-08 13:54:06 -0300

    config project

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { HomePage } from './src/Screens/Home';
+
+export default function App() {
+  return (
+    <HomePage/>
+  );
+}
---
+++ src/Screens/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Title } from  './styles';
+
+export  function HomePage() {
+  return (
+    <Container>
+      <Title> Voce digitou </Title>
+    </Container>
+  );
+}
+
+
